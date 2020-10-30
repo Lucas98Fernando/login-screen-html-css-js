0e411476928ae161b7cec56d89a007c89102527f
Lucas <lucas98fernando@gmail.com>
2020-10-29 21:46:59 -0300

    Adding modal terms and animations improvements

--- js/main.js
+++ js/main.js
@@ -20,3 +20,29 @@ inputs.forEach(input => {
     input.addEventListener('focus', focusFunc);
     input.addEventListener('blur', blurFunc);
 });
+
+// Get the modal
+var modal = document.getElementById("modal-terms");
+
+// Get the button that opens the modal
+var btn = document.getElementById("action-modal");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function () {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
